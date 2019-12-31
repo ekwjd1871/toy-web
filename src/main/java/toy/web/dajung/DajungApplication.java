3418d15bb5addc18db093f9e7dc7a3591cba3811
ekwjd1871 <ekwjd1871@naver.com>
2019-12-31 21:46:06 +0900

    Initial commit

---
+++ src/main/java/toy/web/dajung/DajungApplication.java
@@ -0,0 +1,13 @@
+package toy.web.dajung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DajungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DajungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toy/web/dajung/DajungApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.web.dajung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DajungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

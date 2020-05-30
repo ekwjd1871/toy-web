a6c50c1b4f2c060d5132e2f08a4ce0260ccc38d2
ekwjd1871 <ekwjd1871@naver.com>
2020-05-30 18:59:39 +0900

    userId 세션에서 가져오도록

--- src/main/java/toy/web/dajung/servlet/AddCartService.java
+++ src/main/java/toy/web/dajung/servlet/AddCartService.java
@@ -1,15 +1,18 @@
 package toy.web.dajung.servlet;
 
 import toy.web.dajung.model.Order;
+import toy.web.dajung.model.User;
 import toy.web.dajung.service.ItemDAO;
 import toy.web.dajung.service.OrderDAO;
 import toy.web.dajung.service.UserDAO;
+import toy.web.dajung.utils.SessionUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -19,7 +22,9 @@ public class AddCartService extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String user_id = req.getParameter("user_id");
+        HttpSession session = req.getSession();
+        User loginUser = (User) SessionUtils.getObjectValue(session, "user");
+
         int item_id = Integer.parseInt(req.getParameter("item_id"));
         String buy_count = req.getParameter("buy_count");
 
@@ -29,7 +34,7 @@ public class AddCartService extends HttpServlet {
 
         System.out.println(date_time);
 
-        Order order = new Order(100, user_id, item_id, buy_count, "0", "0", date_time); //order_id는 임의//0은 false 의미
+        Order order = new Order(100, loginUser.getUserId(), item_id, buy_count, "0", "0", date_time); //order_id는 임의//0은 false 의미
 
         try {
             int row = 0;
